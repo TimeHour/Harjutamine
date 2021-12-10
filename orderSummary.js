5e4774107edb5c72eac42cbd05f64e3f8323436c
TimeHour <90251388+TimeHour@users.noreply.github.com>
2021-12-10 09:25:21 +0200

    a

---
+++ orderSummary.js
@@ -0,0 +1,90 @@
+var tellimus = { 
+  "id": 2113, 
+  "orderNumber": "E210126001", 
+  "createdAt": "2021-01-26 16:04:24", 
+  "clientName": "Kalle Tali", 
+  "deliveryType": "Omniva smartpost", 
+  "deliveryValue": 178, 
+  "rows": [ 
+    { 
+      "article_id": 31, 
+      "name": "Kohvimasin De'Longhi Dolce Gusto Piccolo EDG100.W", 
+      "product_code": "257549", 
+      "price": 49.1666666667, 
+      "amount": 1, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23420, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Espresso Intenso, 128 g, 16 tk", 
+      "product_code": "355836", 
+      "price": 5.0666666667, 
+      "amount": 2, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23421, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Latte Macchiato, 194 g, 16 tk", 
+      "product_code": "000000000501267301", 
+      "price": 5.825, 
+      "amount": 1, 
+      "vat": 0.2 
+    } 
+  ] 
+}
+for (var i = 0; i < tellimus["rows"].length; i += 1) {
+         console.log("Name: "+tellimus["rows"][i]["name"]+" Amount: "+ tellimus["rows"][i]["amount"]+" Price: "+ tellimus["rows"][i]["price"]+" Full price: "+ tellimus["rows"][i]["price"] * tellimus["rows"][i]["amount"])
+}
+let sus = 0
+for (var i = 0; i < tellimus["rows"].length; i += 1) {
+       (sus += (tellimus["rows"][i]["price"] * tellimus["rows"][i]["amount"]))
+}
+
+
+
+///////teine variant
+
+
+var tellimus = { 
+  "id": 2113, 
+  "orderNumber": "E210126001", 
+  "createdAt": "2021-01-26 16:04:24", 
+  "clientName": "Kalle Tali", 
+  "deliveryType": "Omniva smartpost", 
+  "deliveryValue": 178, 
+  "rows": [ 
+    { 
+      "article_id": 31, 
+      "name": "Kohvimasin De'Longhi Dolce Gusto Piccolo EDG100.W", 
+      "product_code": "257549", 
+      "price": 49.1666666667, 
+      "amount": 1, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23420, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Espresso Intenso, 128 g, 16 tk", 
+      "product_code": "355836", 
+      "price": 5.0666666667, 
+      "amount": 2, 
+      "vat": 0.2 
+    }, 
+    { 
+      "article_id": 23421, 
+      "name": "Kohvikapslid Nescafe Dolce Gusto Latte Macchiato, 194 g, 16 tk", 
+      "product_code": "000000000501267301", 
+      "price": 5.825, 
+      "amount": 1, 
+      "vat": 0.2 
+    } 
+  ] 
+}
+let sus = 0
+
+for (var i = 0; i < tellimus["rows"].length; i += 1) {
+    let koguSumma = tellimus["rows"][i]["price"] * tellimus["rows"][i]["amount"];
+        console.log("Name: "+tellimus["rows"][i]["name"]+" Amount: "+ tellimus["rows"][i]["amount"]+" Price: "+ tellimus["rows"][i]["price"]+" Full price: "+ koguSumma);
+    (sus += (tellimus["rows"][i]["price"] * tellimus["rows"][i]["amount"]))
+}
+
+console.log("Summ ma on", sus,"ja sellest käibemaks on",sus-sus/1.2)
